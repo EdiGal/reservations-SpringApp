b5437b78d8cc8b074f0244fc050a7e6a36d3394f
= <=>
2018-10-19 11:53:36 +0300

    first commit

---
+++ src/main/java/com/frankmoley/landon/business/domain/RoomReservation.java
@@ -0,0 +1,78 @@
+package com.frankmoley.landon.business.domain;
+
+import java.util.Date;
+
+public class RoomReservation {
+    private long roomId;
+    private long guestId;
+    private String roomType;
+    private int roomFloor;
+    private int roomNumber;
+    private String firstName;
+    private String lastName;
+    private Date date;
+
+    public long getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(long roomId) {
+        this.roomId = roomId;
+    }
+
+    public long getGuestId() {
+        return guestId;
+    }
+
+    public void setGuestId(long guestId) {
+        this.guestId = guestId;
+    }
+
+    public String getRoomType() {
+        return roomType;
+    }
+
+    public void setRoomType(String roomType) {
+        this.roomType = roomType;
+    }
+
+    public int getRoomFloor() {
+        return roomFloor;
+    }
+
+    public void setRoomFloor(int roomFloor) {
+        this.roomFloor = roomFloor;
+    }
+
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(int roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
---
+++ src/main/java/com/frankmoley/landon/data/entity/Room.java
@@ -0,0 +1,55 @@
+package com.frankmoley.landon.data.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="ROOM")
+public class Room {
+    @Id
+    @Column(name="ROOM_ID")
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private long id;
+    @Column(name="TYPE")
+    private String type;
+    @Column(name="FLOOR")
+    private int floor;
+    @Column(name="ROOM_NUMBER")
+    private int roomNumber;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getFloor() {
+        return floor;
+    }
+
+    public void setFloor(int floor) {
+        this.floor = floor;
+    }
+
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(int roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+}
